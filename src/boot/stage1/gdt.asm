41591b4ac9ed85836267afeeeb148665797f9b74
Jacob Abraham <jacob.r.abraham@gmail.com>
2022-02-09 19:53:07 -0700

    fix formatting

--- src/boot/stage1/gdt.asm
+++ src/boot/stage1/gdt.asm
@@ -16,7 +16,7 @@ dw 0xffff ; Limit (bits 0-15)
 dw 0x0 ; Base (bits 0-15)
 db 0x0 ; Base (bits 16-23)
 db 10011010b ; 1st flags, type flags
-db 11001111b ; 2nd flags, Limit (bits 16-19) ; Base (bits 24-31)
+db 11001111b ; 2nd flags, Limit (bits 16-19)
 db 0x0 ;  Base (bits 24-31)
 
 .gdt_data: ; data segement descriptor
@@ -26,7 +26,7 @@ dw 0xffff ; Limit (bits 0-15)
 dw 0x0 ; Base (bits 0-15)
 db 0x0 ; Base (bits 16-23)
 db 10010010b ; 1st flags, type flags
-db 11001111b ; 2nd flags, Limit (bits 16-19) ; Base (bits 24-31)
+db 11001111b ; 2nd flags, Limit (bits 16-19)
 db 0x0 ;  Base (bits 24-31)
 
 .gdt_end:
