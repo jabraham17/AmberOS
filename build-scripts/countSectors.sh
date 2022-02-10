5117e1f2a09c618a3fe9c7d4a50cf10d46d0d00d
Jacob Abraham <jacob.r.abraham@gmail.com>
2022-02-09 18:58:50 -0700

    upgrade build scripts

--- build-scripts/countSectors.sh
+++ build-scripts/countSectors.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 # count number of 512 sectors a binary file will occupy
 
-size=$(wc -c <$1)
-# ceil division
-#(divide+by-1)/by
-nsectors=$(expr $(expr $size + 512 - 1) / 512)
-echo $nsectors
+if [ ! -f $1 ]; then
+    echo "0"
+else
+    size=$(wc -c <$1)
+    # ceil division
+    #(divide+by-1)/by
+    nsectors=$(expr $(expr $size + 512 - 1) / 512)
+    echo $nsectors
+fi
