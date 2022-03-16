c514d570323470eb885ffa986b03a3e03039e0c1
jabraham <jrabraha@asu.edu>
2022-03-16 16:35:57 -0700

    work on frame allocation

--- src/kernel/mm/frame.c
+++ src/kernel/mm/frame.c
@@ -1,28 +1,12 @@
 #include "frame.h"
 
-extern uint32_t __kernel_start;
-extern uint32_t __kernel_start_a;
-uint32_t _kernel_start = (uint32_t)&__kernel_start;
-extern uint32_t __kernel_end;
-uint32_t _kernel_end = (uint32_t)&__kernel_end;
-
-#define _ALIGN_BYTE_OFFSET(_alignment) ((1 << (_alignment)) - 1)
-
-// ALIGN_PTR(_kernel_start, 12)
-#define ALIGN_PTR(_ptr, _alignment)                                            \
-    ((((uint32_t)_ptr) + _ALIGN_BYTE_OFFSET(_alignment)) &                     \
-     ~_ALIGN_BYTE_OFFSET(_alignment))
-
-// ALIGN_PTR(_kernel_start, 0xFFF)
-//   #define ALIGN_PTR(_ptr, _mask)\
-// ((uint32_t)_ptr + (((~((uint32_t)_ptr)) + 1) & (_mask)))
 
 // align _kernel_end to 4k
 void frame_init() {
-    SC_printf("kernel start 0x%x\n", _kernel_start);
-    SC_printf("kernel end   0x%x\n", _kernel_end);
-    SC_printf("kernel start 0x%x\n", &__kernel_start_a);
-    SC_printf("kernel start 0x%x\n", ALIGN_PTR(_kernel_start, 12));
+    SC_printf("kernel start  0x%x\n", _kernel_start);
+    SC_printf("kernel end    0x%x\n", _kernel_end);
+    SC_printf("kernel astart 0x%x\n", ALIGN_PTR(_kernel_start, 12));
+    SC_printf("kernel aend   0x%x\n", ALIGN_PTR(_kernel_end, 12));
 
     // need to align
 }
--- src/kernel/mm/frame.h
+++ src/kernel/mm/frame.h
@@ -3,11 +3,37 @@
 
 #include <stdlib/types.h>
 
-//statically decide max number of frames
-//TODO: make dynamic
+// statically decide max number of frames
+// TODO: make dynamic
 #define NFRAMES 1024
 
-void frame_init();
+uint32_t frames[NFRAMES / 32];
+#define SET_FRAME_BIT(_idx)                                                    \
+    frames[(uint32_t)((_idx) / 32)] |= (0x01 << (uint32_t)((_idx) % 32))
+#define CLEAR_FRAME_BIT(_idx)                                                  \
+    frames[(uint32_t)((_idx) / 32)] &= ~(0x01 << (uint32_t)((_idx) % 32))
+#define GET_FRAME_BIT(_idx)                                                    \
+    (frames[(uint32_t)((_idx) / 32)] & (0x01 << (uint32_t)((_idx) % 32)))
 
+
+
+extern uint32_t __kernel_start;
+uint32_t _kernel_start = (uint32_t)&__kernel_start;
+extern uint32_t __kernel_end;
+uint32_t _kernel_end = (uint32_t)&__kernel_end;
+
+#define _ALIGN_BYTE_OFFSET(_alignment) ((1 << (_alignment)) - 1)
+
+// ALIGN_PTR(_kernel_start, 12)
+#define ALIGN_PTR(_ptr, _alignment)                                            \
+    ((((uint32_t)_ptr) + _ALIGN_BYTE_OFFSET(_alignment)) &                     \
+     ~_ALIGN_BYTE_OFFSET(_alignment))
+
+// ALIGN_PTR(_kernel_start, 0xFFF)
+//   #define ALIGN_PTR(_ptr, _mask)\
+// ((uint32_t)_ptr + (((~((uint32_t)_ptr)) + 1) & (_mask)))
+
+
+void frame_init();
 
 #endif
